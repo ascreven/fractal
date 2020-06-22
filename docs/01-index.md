da286f9468fdde57e13bc70a94c2860ce4ba1dc6
Alexandra Screven <alexandra.screven@gsa.gov>
2020-06-22 13:49:29 -0400

    initial fractal with uswds

---
+++ docs/01-index.md
@@ -0,0 +1,5 @@
+---
+title: Fractal Brown Bag
+---
+
+This is your index page. You can edit its contents at `docs/01-index.njk`
---
+++ fractal.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/*
+* Require the path module
+*/
+const path = require('path');
+
+/*
+ * Require the Fractal module
+ */
+const fractal = module.exports = require('@frctl/fractal').create();
+
+/*
+ * Give your project a title.
+ */
+fractal.set('project.title', 'Fractal Brown Bag');
+
+/*
+ * Tell Fractal where to look for components.
+ */
+const components = fractal.components;
+components.set("ext", ".njk");
+components.set('path', path.join(__dirname, 'components'));
+const nunjucks = require("@frctl/nunjucks");
+components.engine(nunjucks);
+
+/*
+ * Tell Fractal where to look for documentation pages.
+ */
+fractal.docs.set('path', path.join(__dirname, 'docs'));
+
+/*
+ * Tell the Fractal web preview plugin where to look for static assets.
+ */
+fractal.web.set('static.path', path.join(__dirname, 'public'));
